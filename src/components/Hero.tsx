3c207d283f482a98a969cf455b124543b2256b66
kwing25 <kendracwing@icloud.com>
2026-02-18 16:38:30 -0700

    Updating repo...

---
+++ src/components/Hero.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+export default function Hero() {
+  return (
+    <div className="flex min-h-screen items-center  ">
+      <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+        Welcome to my portfolio website!
+      </h1>
+    </div>
+  );
+}
